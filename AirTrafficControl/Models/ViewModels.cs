00862c2a18375788a3cf0cc2b5782afb6ec459bf
Mohammed Eshag <Mesterben21@gmail.com>
2024-10-01 12:15:20 +0300

    Added PaymentReceiptsController and updated ViewModels and Views for payment receipts

---
+++ AirTrafficControl/Models/ViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirTrafficControl.Models
+{
+    public class ViewModels
+    {
+        public int Id { get; set; }
+        public string LicensesType { get; set; }
+        public int LicensesTypeId { get; set; }
+        public int LicenseId { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<decimal> Tax { get; set; }
+        public Nullable<decimal> Stamp { get; set; }
+        public Nullable<decimal> TotalAmount { get; set; }
+        public string PaymentReceiptPath { get; set; }
+      
+    }
+}
